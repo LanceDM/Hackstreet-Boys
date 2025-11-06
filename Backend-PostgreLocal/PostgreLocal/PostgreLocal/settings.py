8bee933ad40994b30b89b00ac2bebd397cb0f369
LanceDM <lancerdm31@gmail.com>
2025-11-07 04:13:32 +0800

    commit

--- Backend-PostgreLocal/PostgreLocal/PostgreLocal/settings.py
+++ Backend-PostgreLocal/PostgreLocal/PostgreLocal/settings.py
@@ -67,7 +67,7 @@ CORS_ALLOW_ALL_ORIGINS = True
 
 # link to front end 
 CORS_ALLOWED_ORIGINS = [
-    "https://hackstreet-boys-1-8e10.onrender.com/", 
+    "https://hackstreet-boys-1-8e10.onrender.com", 
 ]
 
 ROOT_URLCONF = 'PostgreLocal.urls'
@@ -148,8 +148,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_URL = 'static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
